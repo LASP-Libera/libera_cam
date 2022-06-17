b7a7379d2789b2a57db36c56308270b5e7b25ca0
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-06-16 18:39:06 -0600

    Add CLI framework for running l1b processing

---
+++ libera_cam/l1b.py
@@ -0,0 +1,7 @@
+"""Algorithm for WFOV Camera Level 1b product generation"""
+
+
+def algorithm():
+    # TODO: Placeholder. Rename as necessary.
+    print("This would make an L1b product")
+    return
